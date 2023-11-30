18288b67261b30c6e3fd52ab4f675529b6710044
Juan Alejandro Marin Ruiz <amarin@zertifier.com>
2023-11-30 11:44:43 +0100

    feat:save transactions in database

--- src/features/transactions/infrastructure/transactions.module.ts
+++ src/features/transactions/infrastructure/transactions.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { TransactionsController } from './controllers/transactions/transactions.controller';
 import { SharedProvidersModule } from '../../../shared/infrastructure/services/shared-providers.module';
 import { TransactionRepository } from '../domain/TransactionRepository';
-import { InMemoryTransactionRepository } from './repositories/in-memory-transaction-repository/in-memory-transaction-repository';
+import {
+  PrismaTransactionRepository
+} from './repositories/prisma-transaction-repository/prisma-transaction-repository';
 
 @Module({
   imports: [SharedProvidersModule],
   controllers: [TransactionsController],
   providers: [
-    { provide: TransactionRepository, useClass: InMemoryTransactionRepository },
+    { provide: TransactionRepository, useClass: PrismaTransactionRepository },
   ],
 })
 export class TransactionsModule {}
