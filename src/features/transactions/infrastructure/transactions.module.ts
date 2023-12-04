3110876d64db3d67da6757d93ad9010d86e04bd6
Juan Alejandro Marin Ruiz <amarin@zertifier.com>
2023-12-04 11:11:48 +0100

    feat:add transaction migration service

--- src/features/transactions/infrastructure/transactions.module.ts
+++ src/features/transactions/infrastructure/transactions.module.ts
@@ -2,15 +2,15 @@ import { Module } from '@nestjs/common';
 import { TransactionsController } from './controllers/transactions/transactions.controller';
 import { SharedProvidersModule } from '../../../shared/infrastructure/services/shared-providers.module';
 import { TransactionRepository } from '../domain/TransactionRepository';
-import {
-  PrismaTransactionRepository
-} from './repositories/prisma-transaction-repository/prisma-transaction-repository';
+import { PrismaTransactionRepository } from './repositories/prisma-transaction-repository/prisma-transaction-repository';
+import { TransactionMigrationService } from './transaction-migration-service';
 
 @Module({
   imports: [SharedProvidersModule],
   controllers: [TransactionsController],
   providers: [
     { provide: TransactionRepository, useClass: PrismaTransactionRepository },
+    TransactionMigrationService,
   ],
 })
 export class TransactionsModule {}
