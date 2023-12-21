b316087ef43c334ab09c45d48a20f686c8a6283a
Juan Alejandro Marin Ruiz <amarin@zertifier.com>
2023-12-21 09:23:29 +0100

    chore:city entity

--- src/features/cities/domain/City.ts
+++ src/features/cities/domain/City.ts
@@ -1,19 +1,51 @@
 import { CityName } from './CityName';
 import { CityIneCode } from './CityIneCode';
+import { ProvinceId } from '../../provinces/domain/ProvinceId';
+import { CityId } from './CityId';
 
 export class City {
-  private _name: CityName;
-  private _coordinates: any;
-  private _ineCode: CityIneCode;
-
   constructor(params: {
+    id: CityId;
     name: CityName;
     coordinates: any;
     ineCode: CityIneCode;
+    provinceId: ProvinceId;
   }) {
+    this._id = params.id;
     this._name = params.name;
     this._coordinates = params.coordinates;
     this._ineCode = params.ineCode;
+    this._provinceId = params.provinceId;
+  }
+
+  private _id: CityId;
+
+  get id(): CityId {
+    return this._id;
+  }
+
+  private _name: CityName;
+
+  get name(): CityName {
+    return this._name;
+  }
+
+  private _coordinates: any;
+
+  get coordinates(): any {
+    return this._coordinates;
+  }
+
+  private _ineCode: CityIneCode;
+
+  get ineCode(): CityIneCode {
+    return this._ineCode;
+  }
+
+  private _provinceId: ProvinceId;
+
+  get provinceId(): ProvinceId {
+    return this._provinceId;
   }
 
   withIneCode(ineCode: CityIneCode): City {
@@ -28,6 +60,16 @@ export class City {
 
   withCoordinates(coordinates: any): City {
     this._coordinates = coordinates;
+    return this;
+  }
+
+  withProvinceId(provinceId: ProvinceId): City {
+    this._provinceId = provinceId;
+    return this;
+  }
+
+  withId(id: CityId): City {
+    this._id = id;
     return this;
   }
 }
