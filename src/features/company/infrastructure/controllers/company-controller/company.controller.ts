7b6ac16123146ebec8ec8f02fd73f0e0acd51f17
4strodev <jumarurex@gmail.com>
2024-03-15 18:13:12 +0100

    feat:update company

--- src/features/company/infrastructure/controllers/company-controller/company.controller.ts
+++ src/features/company/infrastructure/controllers/company-controller/company.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { CriteriaCodec } from '../../../../../shared/infrastructure/services/criteria-codec/CriteriaCodec';
 import { CompanyRegister } from '../../../application/CompanyRegister';
 import { ListCompanies } from '../../../application/ListCompanies';
@@ -6,6 +6,15 @@ import { HttpResponse } from '../../../../../shared/infrastructure/HttpResponse'
 import { CompanyCriteria } from '../../../domain/CompanyRepository.ts/CompanyCriteria';
 import { CompanyListResponseDTO } from '../DTOs/CompanyListResponseDTO';
 import { CompanyRegisterRequestDTO } from '../DTOs/CompanyRegisterRequestDTO';
+import { CompanyUpdate } from '../../../application/company-update';
+import { Company } from '../../../domain/Company';
+import { CompanyId } from '../../../domain/CompanyID/CompanyId';
+import { CompanySize } from '../../../domain/CompanySize';
+import { CompanyCIF } from '../../../domain/CompanyCIF';
+import { CompanyName } from '../../../domain/CompanyName';
+import { CompanyDescription } from '../../../domain/CompanyDescription';
+import { Sector } from '../../../domain/Sector';
+import { CompanyPhone } from '../../../domain/Phone/CompanyPhone';
 
 @Controller('company')
 export class CompanyController {
@@ -13,6 +22,7 @@ export class CompanyController {
     private companyRegister: CompanyRegister,
     private criteriaCodec: CriteriaCodec,
     private listCompanies: ListCompanies,
+    private companyUpdate: CompanyUpdate,
   ) {}
 
   @Get()
@@ -32,7 +42,19 @@ export class CompanyController {
   }
 
   @Put(':id')
-  async updateCompany() {
-
+  async updateCompany(
+    @Body() body: CompanyRegisterRequestDTO,
+    @Param('id') id: string,
+  ) {
+    const company = new Company({
+      id: new CompanyId(id),
+      companySize: CompanySize.from(body.companySize),
+      CIF: new CompanyCIF(body.CIF),
+      name: new CompanyName(body.name),
+      description: new CompanyDescription(body.description || ''),
+      sector: Sector.from(body.sector),
+      phone: new CompanyPhone(body.phone),
+    });
+    await this.companyUpdate.run(company);
   }
 }
